c10913a3d825a14b8cd94b2b8dc6210712cd0f9d
oys1205 <yosep.ciafirst@gmail.com>
2025-04-11 10:06:01 +0900

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# 파일 이름
+FILE_NAME="deploy.sh"
+
+# 메시지
+MESSAGE="Testing"
+
+# 시간 (현재 날짜 + 시간)
+TIME=$(date "+%Y-%m-%d %H:%M:%S (KST)")
+
+# 상태
+STATUS="now"
+
+# 출력
+printf "📄 %-15s  %-30s  %s\n" "$FILE_NAME" "$MESSAGE | $TIME" "$STATUS"
+
 git add .
 git commit -m "updated"
 git push
